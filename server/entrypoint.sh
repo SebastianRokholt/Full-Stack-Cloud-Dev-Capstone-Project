eec61cd7c565b1c30c2a528f25c371a5c886b189
SebastianRokholt <sebastianrokholt@hotmail.com>
2022-02-28 14:05:07 +0000

    kubernetes deployment config adjustment
    fixed pod CrashLoopBackoff issue

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,17 +1,17 @@
  #!/bin/sh
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Waiting for postgres..."
 
-    if [ "$DATABASE" = "postgres" ]; then
-        echo "Waiting for postgres..."
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+    sleep 0.1
+    done
 
-        while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-        sleep 0.1
-        done
+    echo "PostgreSQL started"
+fi
 
-        echo "PostgreSQL started"
-    fi
+# Make migrations and migrate the database.
+echo "Making migrations and migrating the database. "
 
-    # Make migrations and migrate the database.
-    echo "Making migrations and migrating the database. "
-    python manage.py makemigrations main --noinput 
-    python manage.py migrate --noinput 
-    exec "$@"
+python manage.py makemigrations main --noinput 
+python manage.py migrate --noinput 
+exec "$@"
